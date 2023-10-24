8ae752a879597ed1fb5d16db770e67e908ade0c7
hiikaruu <yukko.moriyama@gmail.com>
2023-10-25 01:29:26 +0300

    idk

---
+++ src/main/java/com/example/dockerjenkinsproj/DockerJenkinsProjApplication.java
@@ -0,0 +1,13 @@
+package com.example.dockerjenkinsproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerJenkinsProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerJenkinsProjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dockerjenkinsproj/DockerJenkinsProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dockerjenkinsproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerJenkinsProjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
