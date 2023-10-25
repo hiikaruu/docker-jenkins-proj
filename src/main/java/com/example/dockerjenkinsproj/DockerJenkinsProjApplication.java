57fb24527ac4563da4649f5322d996402b4f5599
hiikaruu <yukko.moriyama@gmail.com>
2023-10-25 18:05:06 +0300

    idk

--- src/main/java/com/example/dockerjenkinsproj/DockerJenkinsProjApplication.java
+++ src/main/java/com/example/dockerjenkinsproj/DockerJenkinsProjApplication.java
@@ -1,9 +1,4 @@
 package com.example.dockerjenkinsproj;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
 public class DockerJenkinsProjApplication {
 
 	public static void main(String[] args) {
